1dc88ce490f09b2770d9cfae6160268439e0c4df
mykeldj <43410772+mykeldj@users.noreply.github.com>
2018-09-25 13:46:23 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 #variables with user credentials for twitter API access
-consumer_key = 'bSe8AmtetE71yq6YUb9lpp1cV'
-consumer_secret = 'BOoajwtnUCRr8OySPqwzXSORSK5MfTvLWX4hmQDClJRHgw09pL'
-access_token = '172076221-gkD3dIaa5i3SUMx8HPLZnA3rsWtoKpmrcOPGeywU'
-access_token_secret = 'N2ZIvGgNHFsmnLUmTf4Y4Dh7RVZdoW7SNvcxmYPkIIWDg'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
